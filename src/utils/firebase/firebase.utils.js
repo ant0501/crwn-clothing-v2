8d2cf2a83c2eabefcdf4095c2ee16b3b9c300a0f
ant0501 <hui.wang@syd.healthmore.com>
2022-07-02 09:23:53 +1000

    9

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,13 +9,21 @@ import {
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+//   authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+//   projectId: 'crwn-clothing-db-98d4d',
+//   storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+//   messagingSenderId: '626766232035',
+//   appId: '1:626766232035:web:506621582dab103a4d08d6',
+// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCAKidwrq2fyEtl9CONPMgtUBunIcs9A5M",
+  authDomain: "crown-clothing-db-f774b.firebaseapp.com",
+  projectId: "crown-clothing-db-f774b",
+  storageBucket: "crown-clothing-db-f774b.appspot.com",
+  messagingSenderId: "1061213338412",
+  appId: "1:1061213338412:web:bd5b979c44530589640a89"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
