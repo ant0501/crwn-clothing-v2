4ee9879d253a3d093b9c1958e868f9583c81426b
ant0501 <hui.wang@syd.healthmore.com>
2022-08-24 00:37:57 +1000

    add checkout page

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,13 +2,14 @@ import "./cart-item.styles.scss";
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
+  console.log(name, imageUrl, price, quantity);
   return (
     <div className="cart-item-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
-          {quantity} x ${price}
+          {quantity} × ${price}
         </span>
       </div>
     </div>
